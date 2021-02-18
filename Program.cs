dcd5ab7e5a1bcea1ea83add6c9fb71330d0ca2d5
parkyounghoo <32727007+parkyounghoo@users.noreply.github.com>
2021-02-18 15:56:46 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace hrdNet
 {
     class Program
     {
-        //static string connectionString = "server = localhost; uid = sa; pwd = 1111; database = PrivateData;";
-        static string connectionString = "server = 10.200.5.73,1477; uid = savewind; pwd = savewind11!; database = CENTER_RAW;";
+        static string connectionString = "server = localhost; uid = sa; pwd = 1111; database = PrivateData;";
         static void Main(string[] args)
         {
             string date = "";
